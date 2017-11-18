724ce68d113eb3677cd12ed8cb64b83891c54f00
Alexander Casal <alexandericasal@gmail.com>
2017-11-18 12:24:28 -0700

    Add UriMatcher to route incoming Uri requests

--- app/src/main/java/com/alexander/udacity/udacity_pets_sqlite/data/PetProvider.kt
+++ app/src/main/java/com/alexander/udacity/udacity_pets_sqlite/data/PetProvider.kt
@@ -2,6 +2,7 @@ package com.alexander.udacity.udacity_pets_sqlite.data
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 
@@ -9,6 +10,17 @@ class PetProvider : ContentProvider() {
 
     private val TAG = PetProvider::class.java.simpleName
     private lateinit var mDbHelper: PetDbHelper
+
+    companion object {
+        const val PETS = 100
+        const val PET_ID = 1001
+        val sUriMatcher = UriMatcher(UriMatcher.NO_MATCH)
+
+        init {
+            sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS)
+            sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, "${PetContract.PATH_PETS}/#", PET_ID)
+        }
+    }
 
     override fun onCreate(): Boolean {
         mDbHelper = PetDbHelper(context)
